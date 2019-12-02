17c92645d59421b270eeb1bc35de4278c30d97e4
Marco Vasapollo <ceo@metaring.com>
2019-12-02 08:43:35 +0100

    Series toJSON Bugfix

--- src-gen/com/metaring/framework/functionality/FunctionalityErrorTypeEnumeratorSeries.java
+++ src-gen/com/metaring/framework/functionality/FunctionalityErrorTypeEnumeratorSeries.java
@@ -173,8 +173,11 @@ public class FunctionalityErrorTypeEnumeratorSeries extends ArrayList<Functional
     @Override
     public String toJson() {
         StringBuilder sb = new StringBuilder("[");
-        forEach(it -> sb.append(it.toJson()).append(","));
-        return sb.delete(sb.length() - 1, sb.length()).append("]").toString();
+        if(!isEmpty()) {
+            forEach(it -> sb.append(it.toJson()).append(","));
+            sb.delete(sb.length() - 1, sb.length());
+        }
+        return sb.append("]").toString();
     }
 
     public DataRepresentation toDataRepresentation() {
